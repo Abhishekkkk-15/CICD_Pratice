ca2df2de8715313589804d8ed2b5f59251417e4c
abhishekkkk-15 <mrabhi748@gmail.com>
2025-05-24 19:08:21 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+    const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(express.json());
+
+// Route
+app.get('/', (req, res) => {
+  res.send('Hello from Express!');
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
