346f7a33a3a6db1c5b516a22e77b845c2326e48d
abhishekkkk-15 <mrabhi748@gmail.com>
2025-05-24 21:51:00 +0530

    msg changes

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
-    const express = require('express');
+const express = require("express");
 const app = express();
 const PORT = 4002;
 
-// Middleware
 app.use(express.json());
 
-// Route
-app.get('/', (req, res) => {    
-  res.json({message:"Hey there"});
+app.get("/", (req, res) => {
+  res.json({ message: "Hey there" });
 });
 
-// Start server
-app.listen(PORT,'0.0.0.0', () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
