52923e04d52513d1403b72959d0d970d4bc16a29
abhishekkkk-15 <mrabhi748@gmail.com>
2025-05-25 12:30:36 +0530

    index.html added

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 const PORT = 4002;
 
 app.use(express.json());
 
+app.use(express.static(path.join(__dirname, "public")));
+
 app.get("/", (req, res) => {
-  res.json({ message: "CI/CD pipeline" });
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
 app.listen(PORT, "0.0.0.0", () => {
